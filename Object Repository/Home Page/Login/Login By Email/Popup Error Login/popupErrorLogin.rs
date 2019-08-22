4b2989fc57e8d6f98be3eb970055850ce9cb064c
Tika Ayu <tika.ayu@mataharimall.com>
2019-08-22 11:12:10 +0700

    popup error

---
+++ Object Repository/Home Page/Login/Login By Email/Popup Error Login/popupErrorLogin.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>popupErrorLogin</name>
+   <tag></tag>
+   <elementGuidId>49f5703d-c3c7-453a-87f1-20821f242db2</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[@class='swal2-popup swal2-modal swal2-show']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class='swal2-popup swal2-modal swal2-show']</value>
+   </webElementProperties>
+</WebElementEntity>
